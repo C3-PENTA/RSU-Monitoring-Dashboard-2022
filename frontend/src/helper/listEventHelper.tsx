a111f3735d3a82fc63e7d2592029fd8c670b9253
YunSunW00 <swyun@pentasecurity.com>
2022-08-12 13:36:16 +0900

    modify: listEventHelper

--- frontend/src/helper/listEventHelper.tsx
+++ frontend/src/helper/listEventHelper.tsx
@@ -19,3 +19,56 @@ import { safeAnyToNumber } from '@/util/primitiveHandle';
 export const getCategoryConfig = (category: TypesOf<IListEvent>) =>
   categoryConfig.find(({ id }) => id === category);
 
+export const renderEventInfo = (data: IListEvent, disableTooltip?: boolean) => {
+  const category = getCategoryConfig(data.category);
+  switch (category?.id) {
+    case 1: {
+      const eventInfoData = data.eventInfo as IEventInfoAvailability;
+      const traffic = Math.round((eventInfoData.nic.tx / eventInfoData.nic.rx) * 100);
+      return (
+        <Tooltip
+          className="text-ellipsis--2"
+          label={`CPU ${eventInfoData.cpu}%, Memory ${eventInfoData.ram}%, Traffic volume ${traffic}%`}
+          disabled={disableTooltip}
+        >
+          CPU (
+          <span className={`text-${eventInfoData.cpu < CPUThreshold ? 'green' : 'red'}`}>
+            {eventInfoData.cpu}%
+          </span>
+          ), Memory (
+          <span className={`text-${eventInfoData.ram < RAMThreshold ? 'green' : 'red'}`}>
+            {eventInfoData.ram}%
+          </span>
+          ),
+          <br />
+          Current traffic volume (
+          <span className={`text-${traffic < NICThreshold ? 'green' : 'red'}`}>{traffic}%</span>)
+        </Tooltip>
+      );
+    }
+    case 2: {
+      return (
+        <div className="table__event-info text-ellipsis--2">
+          <>
+            {t('list_event.info.virus', {
+              fileName: (data.eventInfo as IEventInfoVirus).fileName || '',
+            })}
+          </>
+        </div>
+      );
+    }
+    case 3:
+      return (
+        <>
+          {t('list_event.info.communication', {
+            detectionNode: data.detectionNode || '',
+            info: (data.eventInfo as IEventInfoCommunication).info || '',
+          })}
+        </>
+      );
+
+    default:
+      return <></>;
+  }
+};
+
