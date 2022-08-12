0c01449aa2849dd0c9a927d2ef92fcd413201473
YunSunW00 <swyun@pentasecurity.com>
2022-08-12 13:34:44 +0900

    Add: listEventHelper.tsx

---
+++ frontend/src/helper/listEventHelper.tsx
@@ -0,0 +1,18 @@
+import { Tooltip } from '@mantine/core';
+import dayjs from 'dayjs';
+import { t } from 'i18next';
+
+import { categoryConfig, listEventTableConfig } from '@/config/listEventConfig';
+import { CPUThreshold, NICThreshold, paginationConfig, RAMThreshold } from '@/config/system';
+import {
+  IEventInfoAvailability,
+  IEventInfoCommunication,
+  IEventInfoVirus,
+  IListEvent,
+  IListEventForm,
+  IListEventReq,
+} from '@/interface/interfaceListEvent';
+import { TypesOf } from '@/interface/interfaceCommon';
+import { checkStrArrIncludeKeyword } from '@/util/arrayHandle';
+import { safeAnyToNumber } from '@/util/primitiveHandle';
+
