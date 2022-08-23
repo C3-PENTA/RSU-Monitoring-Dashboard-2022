45ad3450c02f057a39b39197395672d600cfe5b1
YunSunW00 <swyun@pentasecurity.com>
2022-08-23 09:11:41 +0900

    Add: OBU entity

--- backend/src/modules/node/entity/obu.entity.ts
+++ backend/src/modules/node/entity/obu.entity.ts
@@ -1,1 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Rsu } from './rsu.entity';
+
+
