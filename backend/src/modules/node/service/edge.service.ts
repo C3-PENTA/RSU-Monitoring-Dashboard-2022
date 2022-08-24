320907c0430a37689f7c83d8f08d7297134bd5a0
YunSunW00 <swyun@pentasecurity.com>
2022-08-24 10:20:45 +0900

    Modify:edge.service

--- backend/src/modules/node/service/edge.service.ts
+++ backend/src/modules/node/service/edge.service.ts
@@ -4,3 +4,23 @@ import { Repository } from 'typeorm';
 import { ListNodeDto } from '../dto/list-node.dto';
 import { Edge } from '../entity/edge.entity';
 
+@Injectable()
+export class EdgeService {
+  constructor(
+    @InjectRepository(Edge) private edgeRepository: Repository<Edge>,
+  ) {}
+
+  /**
+   * Find all Edges with corresponding RSUs, OBUs
+   * @returns {Promise<ListNodeDto>}
+   */
+  async findAll(): Promise<ListNodeDto> {
+    const listEdge = await this.edgeRepository
+      .createQueryBuilder('edge')
+      .leftJoinAndSelect('edge.listRsu', 'rsu')
+      .leftJoinAndSelect('rsu.listObu', 'obu')
+      .orderBy('edge.name', 'ASC')
+      .getMany();
+    return { listEdge };
+  }
+}
