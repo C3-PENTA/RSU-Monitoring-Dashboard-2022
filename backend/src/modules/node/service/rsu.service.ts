49b9a087b0b77aecab5e9547b6ee8aeeb3909ee7
YunSunW00 <swyun@pentasecurity.com>
2022-08-24 10:21:14 +0900

    Add: rsu.service

---
+++ backend/src/modules/node/service/rsu.service.ts
@@ -0,0 +1,6 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Rsu } from '../entity/rsu.entity';
+
+
