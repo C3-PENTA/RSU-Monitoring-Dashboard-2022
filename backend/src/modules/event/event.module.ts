5dd6a7bc23f114be34d310b6b057fc130529725d
YunSunW00 <swyun@pentasecurity.com>
2022-08-26 12:15:17 +0900

    Add: event.modules

--- backend/src/modules/event/event.module.ts
+++ backend/src/modules/event/event.module.ts
@@ -5,3 +5,10 @@ import { EventController } from './controller/event.controller';
 import { Event } from './entity/event.entity';
 import { EventService } from './service/event.service';
 
+@Module({
+  imports: [TypeOrmModule.forFeature([Event]), NodeModule],
+  controllers: [EventController],
+  providers: [EventService],
+  exports: [EventService],
+})
+export class EventModule {}
