96b1be34096cdc6adab99c47f5e22f659a1baf73
YunSunW00 <swyun@pentasecurity.com>
2022-08-26 12:15:08 +0900

    Add: event.modules

---
+++ backend/src/modules/event/event.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NodeModule } from '../node/node.module';
+import { EventController } from './controller/event.controller';
+import { Event } from './entity/event.entity';
+import { EventService } from './service/event.service';
+
