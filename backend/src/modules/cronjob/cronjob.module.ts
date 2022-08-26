f6f6bac4589885bfd5894ffad1712a25a2c46eb7
YunSunW00 <swyun@pentasecurity.com>
2022-08-26 11:13:21 +0900

    Modify: cronjob

--- backend/src/modules/cronjob/cronjob.module.ts
+++ backend/src/modules/cronjob/cronjob.module.ts
@@ -2,3 +2,8 @@ import { Module } from '@nestjs/common';
 import { EventModule } from '../event/event.module';
 import { CronjobService } from './service/cronjob.service';
 
+@Module({
+  imports: [EventModule],
+  providers: [CronjobService],
+})
+export class CronjobModule {}
