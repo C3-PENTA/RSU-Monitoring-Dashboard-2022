e470ef3f317f25567a42c3c7558ae76b825c0951
YunSunW00 <swyun@pentasecurity.com>
2022-08-26 12:14:15 +0900

    Modify: cronjob.service

--- backend/src/modules/cronjob/service/cronjob.service.ts
+++ backend/src/modules/cronjob/service/cronjob.service.ts
@@ -16,3 +16,26 @@ export class CronjobService {
   /**
    * Job clear "Node Availability Status Transfer" Event 3 months ago
    */
+  @Cron('0 0 0 * * *')
+  async handleJobClearDataEvent() {
+    try {
+      const today = new Date().toISOString();
+      this.logger.log('Job run: ' + today);
+
+      const TIME_INTERVAL_CLEAR_DATA_EVENT =
+        this.configService.get<number>('TIME_INTERVAL_CLEAR_DATA_EVENT') || 3;
+
+      const timeClearDataEvent = moment(today)
+        .subtract(TIME_INTERVAL_CLEAR_DATA_EVENT, 'months')
+        .toISOString();
+      this.logger.log('timeClearDataEvent: ' + timeClearDataEvent);
+
+      const result = await this.eventService.deleteBeforeTime(
+        timeClearDataEvent,
+      );
+      this.logger.log('result: ' + JSON.stringify(result));
+    } catch (e) {
+      this.logger.error('Clear data event error: ', (e as Error).stack);
+    }
+  }
+}
