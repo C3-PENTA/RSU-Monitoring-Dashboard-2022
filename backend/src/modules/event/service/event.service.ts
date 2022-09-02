40eff3db7b8c12784572efb5359ae3bd27f760a4
YunSunW00 <swyun@pentasecurity.com>
2022-09-02 15:15:04 +0900

    Add: event service

---
+++ backend/src/modules/event/service/event.service.ts
@@ -0,0 +1,4 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RsuService } from '../../../modules/node/service/rsu.service';
