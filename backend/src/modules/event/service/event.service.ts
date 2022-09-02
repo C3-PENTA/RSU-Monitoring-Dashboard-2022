7c1add3cdd9b1d50c5e75d3c4a48ee4a05b95a7f
YunSunW00 <swyun@pentasecurity.com>
2022-09-02 15:16:04 +0900

    Modify: event service

--- backend/src/modules/event/service/event.service.ts
+++ backend/src/modules/event/service/event.service.ts
@@ -10,3 +10,12 @@ import {
   SelectQueryBuilder,
   WhereExpressionBuilder,
 } from 'typeorm';
+import { EventDataDto } from '../dto/event-data.dto';
+import { QueryLoadMoreEventBodyDto } from '../dto/query-load-more-event-body.dto';
+import { QueryPagingEventBodyDto } from '../dto/query-paging-event-body.dto';
+import { Event } from '../entity/event.entity';
+import { CategoryEnum } from '../enum/category.enum';
+import { PaginationEventDto } from '../dto/pagination-event.dto';
+import { LoadMoreEventDto } from '../dto/load-more-event.dto';
+import { QuerySearchEventBodyDto } from '../dto/query-search-event-body.dto';
+
