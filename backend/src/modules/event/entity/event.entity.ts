d92b205cee8726a3fe468321265fc1f3038791cf
YunSunW00 <swyun@pentasecurity.com>
2022-09-02 15:12:48 +0900

    Modify: event entity

--- backend/src/modules/event/entity/event.entity.ts
+++ backend/src/modules/event/entity/event.entity.ts
@@ -1,1 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { EventInfo } from '../type/event-info.type';
+
