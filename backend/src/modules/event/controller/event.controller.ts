114e038f28bbaf6343c1813cab16f2cd7e423c02
YunSunW00 <swyun@pentasecurity.com>
2022-09-02 15:10:15 +0900

    Add: event controller

---
+++ backend/src/modules/event/controller/event.controller.ts
@@ -0,0 +1,8 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { PaginationEventDto } from '../dto/pagination-event.dto';
+import { QueryPagingEventBodyDto } from '../dto/query-paging-event-body.dto';
+import { QueryLoadMoreEventBodyDto } from '../dto/query-load-more-event-body.dto';
+import { EventService } from '../service/event.service';
+import { LoadMoreEventDto } from '../dto/load-more-event.dto';
+
