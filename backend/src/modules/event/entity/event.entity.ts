ef45d9f14cacb78ce7fab4e48c514674edc78c22
YunSunW00 <swyun@pentasecurity.com>
2022-09-02 15:13:21 +0900

    Modify: event entity

--- backend/src/modules/event/entity/event.entity.ts
+++ backend/src/modules/event/entity/event.entity.ts
@@ -18,5 +18,26 @@ export class Event {
   @Column()
   category: number;
 
+  @Column()
+  sendNodeType: string;
+
+  @ApiProperty()
+  @Column()
+  sendNode: string;
+
+  @Column({ default: () => `''` })
+  receiveNodeType: string;
+
+  @ApiProperty()
+  @Column({ default: () => `''` })
+  receiveNode: string;
+
+  @Column({ default: () => `''` })
+  detectionNodeType: string;
+
+  @ApiProperty()
+  @Column({ default: () => `''` })
+  detectionNode: string;
+
 
 }
