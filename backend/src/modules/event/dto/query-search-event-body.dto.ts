1479f5dfe338678394b3de617608a8eef8711180
YunSunW00 <swyun@pentasecurity.com>
2022-09-07 09:26:09 +0900

    Modify: new file

--- backend/src/modules/event/dto/query-search-event-body.dto.ts
+++ backend/src/modules/event/dto/query-search-event-body.dto.ts
@@ -1,3 +1,11 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsNumber, IsOptional, IsString, IsDateString } from 'class-validator';
 
+export class QuerySearchEventBodyDto {
+  @ApiProperty({
+    type: [Number],
+    description:
+      '1 - Node Availability Status Transfer Event, 2 - Virus Detection Event, 3 - Node Communication Event',
+  })
+
+}
