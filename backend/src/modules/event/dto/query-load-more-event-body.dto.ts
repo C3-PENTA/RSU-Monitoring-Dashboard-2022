0abc6b6630a05975a8c8a1ade6b4ae8701989a65
YunSunW00 <swyun@pentasecurity.com>
2022-09-07 09:23:27 +0900

    Add: new file

---
+++ backend/src/modules/event/dto/query-load-more-event-body.dto.ts
@@ -0,0 +1,3 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Min, IsNumber, IsOptional, IsDateString } from 'class-validator';
+import { QuerySearchEventBodyDto } from './query-search-event-body.dto';
