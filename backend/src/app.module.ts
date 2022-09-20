b56dffb5aa1242ad442008b67d9cd76769110a18
YunSunW00 <swyun@pentasecurity.com>
2022-09-20 15:36:25 +0900

    Add: app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,3 +7,13 @@ import { EventModule } from './modules/event/event.module';
 import { AppGateway } from './app.gateway';
 import { ScheduleModule } from '@nestjs/schedule';
 import { CronjobModule } from './modules/cronjob/cronjob.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+
+})
+export class AppModule {}
