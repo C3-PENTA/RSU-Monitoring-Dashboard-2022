8fcb0e6563993334ee9131a2d155169d9d79c822
YunSunW00 <swyun@pentasecurity.com>
2022-09-20 15:35:59 +0900

    Add: app.module

---
+++ backend/src/app.module.ts
@@ -0,0 +1,4 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './config/database/typeorm-config.service';
