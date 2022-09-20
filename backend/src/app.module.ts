8efe134f6ca95b6b05b0b4e438b2a3f66f485262
YunSunW00 <swyun@pentasecurity.com>
2022-09-20 15:36:09 +0900

    Add: app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,3 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './config/database/typeorm-config.service';
+import { NodeModule } from './modules/node/node.module';
+import { EventModule } from './modules/event/event.module';
+import { AppGateway } from './app.gateway';
+import { ScheduleModule } from '@nestjs/schedule';
+import { CronjobModule } from './modules/cronjob/cronjob.module';
