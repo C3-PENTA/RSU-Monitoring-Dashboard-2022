90c43bf118727e8ec678a04615405b2f246031c0
YunSunW00 <swyun@pentasecurity.com>
2022-09-20 15:40:24 +0900

    Add: main file

---
+++ backend/src/main.ts
@@ -0,0 +1,6 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
+import { AppModule } from './app.module';
