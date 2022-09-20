c7c981745d6d225726c60bac852addb4d188c313
YunSunW00 <swyun@pentasecurity.com>
2022-09-20 15:40:42 +0900

    Add: main file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,3 +4,13 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as fs from 'fs';
 import { AppModule } from './app.module';
+
+async function bootstrap() {
+  let httpsOptions: { key: Buffer; cert: Buffer };
+  if (process.env.NODE_ENV !== 'development') {
+    httpsOptions = {
+      key: fs.readFileSync('./gmoseapp.key'),
+      cert: fs.readFileSync('./gmoseapp.csr'),
+    };
+  }
+
