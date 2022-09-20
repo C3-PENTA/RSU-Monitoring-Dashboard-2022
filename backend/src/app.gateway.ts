c24911115111a4d274584878cc3ecbeccd0df59e
YunSunW00 <swyun@pentasecurity.com>
2022-09-20 15:32:55 +0900

    Modify: app.gateway

--- backend/src/app.gateway.ts
+++ backend/src/app.gateway.ts
@@ -12,3 +12,7 @@ import { EventService } from './modules/event/service/event.service';
 import { ConfigService } from '@nestjs/config';
 import { CategoryEnum } from './modules/event/enum/category.enum';
 import { EventDataDto } from './modules/event/dto/event-data.dto';
+import { RsuService } from './modules/node/service/rsu.service';
+import { Event } from './modules/event/entity/event.entity';
+import 'dotenv/config';
+
